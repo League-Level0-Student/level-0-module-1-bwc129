982b74df22d492d7a658221389e642171725ccd9
league <league@192.168.1.74>
2024-01-29 16:53:17 -0800

    jjjjj

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,20 @@
-
+PImage pepperoni;
+  
+  
+  
 void setup() {
-    
+ size(2000, 2000); 
+ fill(#FEFF03);
+ pepperoni = loadImage("pepperoni.png");
+ellipse(1000, 475, 1500, 1000);
+
+fill(#FF0303);
+ellipse(1000,475, 900, 750);
 }
 void draw() {
-    
+
+  image (pepperoni, mouseX, mouseY);
 }
+
+  
+    
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,24 +2,29 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String riddle =  JOptionPane.showInputDialog("Tear one off and scratch my head what was red is black instead.");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (riddle.equals ("a match")) {
+	String yes =  JOptionPane.showInputDialog("correct your right."); }
+int score ++1
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+	else {
+		String no =  JOptionPane.showInputDialog("wrong you are."); }
+	}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
 		
-	}
+	
 }
 
+}
