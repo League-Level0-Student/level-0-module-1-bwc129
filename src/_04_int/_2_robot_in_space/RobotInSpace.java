3b9ede8247ab5a5bb5d03d958cfff0cc11534d8d
league <league@192.168.1.74>
2024-02-19 16:59:42 -0800

    hi

--- src/_04_int/_2_robot_in_space/RobotInSpace.java
+++ src/_04_int/_2_robot_in_space/RobotInSpace.java
@@ -34,13 +34,22 @@ public class RobotInSpace extends KeyboardAdapter {
 		// each arrow key
 		System.out.println(keyPressed);
 		// 3. If the up arrow is pressed, move the Robot up the screen.
-		if keyPressed ==38;
+	if (keyPressed ==38) {
+		rob.move(5);
+	}
+	
 		// 4. If the down arrow is pressed, move the Robot down.
-		
+	if (keyPressed ==39) {
+		rob.turn(5);
+	}
 		// 5. If the left arrow is pressed, make the Robot go left.
-		
+	if (keyPressed ==40) {
+		rob.move(-5);
+	}
 		// 6. If right is pressed, move the Robot right.
-		
+	if (keyPressed ==37) {
+		rob.turn(-5);
+	}
 		// 7. Run your program and move the Robot to R2-D2 for a surprise!
 		
 		
--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,20 @@
 
 void setup() {
+size(500,500);
+PImage face = loadImage("dogggy.jpeg");
+face.resize(500, 500);
+image(face, 0, 0);
+fill(100,200,130);
+ellipse(170, 195, 40, 40);
+ellipse(330, 185, 40, 40);
+
 
 }
 void draw() {
-
+println(mouseX);
+  if (mouseX>=100 && mouseX<=120 && mouseY>=130 && mouseY<=150) {
+fill(2,100,300);
+ellipse(170, 195, 40, 40);
+ellipse(330, 185, 40, 40);
+}
 }
